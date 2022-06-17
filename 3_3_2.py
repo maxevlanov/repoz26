a5f1f572f4aa264a4dd0c7750707d93ccc88571e
maxevlanov <max.57207@mail.ru>
2022-06-17 11:21:26 +0300

    initial commit

---
+++ 3_3_2.py
@@ -0,0 +1,4 @@
+name = input("Введите своё имя в И.п.: ")
+age = input("Введите своё возраст в виде числа: ")
+city = input("Введите название своего города: ")
+print("Меня зовут {name}. Мне {age} лет. Я живу в городе {city}.".format(name = name, age = age, city = city))
