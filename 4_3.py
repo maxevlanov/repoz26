ac7db9e9bea79d44ef114aa6677957d349d3b02b
maxevlanov <max.57207@mail.ru>
2022-06-21 15:18:39 +0300

    initial commit

--- 4_3.py
+++ 4_3.py
@@ -1,2 +1,13 @@
 """Заполнить словарь где ключами будут выступать числа от 0 до n, а значениями вложенный словарь с ключами "name" и "email", а значения
 для этих ключей будут браться с клавиатуры"""
+n: int = int(input("Введите количество ключей: "))
+my_dict2: dict = {}
+my_dict1: dict = {}
+for i in range(1, n + 1):
+    my_dict1[i] = "name"
+    name = str(input("Введите имя: "))
+for j in range(1, n + 1):
+    my_dict2[j] = "email"
+    email = str(input("Введите адрес электронной почты: "))
+print(my_dict1)
+print(my_dict2)
