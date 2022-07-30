f837d31fb2429d67a9ccb43935b1cddb1b39247a
maxevlanov <max.57207@mail.ru>
2022-07-30 11:14:45 +0300

    initial commit

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -4,8 +4,8 @@ from .order_item import *
 from .order import *
 from .status import *
 from .bot_user import *
-from invoice import *
-from language import *
+from .invoice import *
+from .language import *
 
 from .config import *
 from .user import *
